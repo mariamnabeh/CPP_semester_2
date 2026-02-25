9a06d7d71f934c5ff694052860c3d16f316df1fd
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-02-25 09:00:58 +0200

    update

---
+++ exercises/tut_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+using namespace std;
+
+#define el "\n"
+typedef long long ll;
+typedef long double ld;
+
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main()
+{
+int arr[3]={1,3,4};
+cout<<arr[0];
+
+
+
+}
