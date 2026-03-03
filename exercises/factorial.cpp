5ef592d4129522cc2fc2ba5bcaaf54acf923c34f
mariammohamedd2 <141452298+mariamnabeh@users.noreply.github.com>
2026-03-03 16:15:58 +0200

    How loops works (Update)

--- exercises/factorial.cpp
+++ exercises/factorial.cpp
@@ -8,11 +8,28 @@ using namespace std;
 int main(){
     int res=1;
     int num;
+    bool condtion =1;
+
     cout<<"Inter num" <<el;
     cin>>num;
     int x=1; //modification
-    for (; x<=num;x++){
+    for (; condtion;){
+        condtion=(x<num);
         res*=x;
+        x++;
+        //condtion=(x<num);
+/*
+ 0) Initialization (happens once before the loop starts)
+
+1)Check the condition
+
+2) If the condition is true → execute the loop body
+If false → exit the loop
+
+3) Increase / decrease (update the variable)
+
+4) Go back to step 2 and check the condition again 
+*/
     }
 cout<<"factorial("  <<num<< ") =" <<res<<el;
 
